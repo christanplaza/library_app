260f95558e77abc8672e419b62550513ab2fa109
Christan Shane Plaza <christanshaneplaza@gmail.com>
2024-09-09 19:37:52 +0800

    Create Book

--- library/urls.py
+++ library/urls.py
@@ -4,8 +4,8 @@ from . import views
 # All URLS after 'library/<urls here>'
 urlpatterns = [
   path('books', views.index, name='index'),
-  path('books/<id>', views.book_detail, name='book-detail'),
-  path('create-book', views.create, name='create-book'),
+  path('books/<id>/', views.book_detail, name='book-detail'),
+  path('books/create', views.create, name='create-book'),
   path('update-book', views.update, name='update-book'),
   path('delete-book', views.delete, name='delete-book'),
 ]
